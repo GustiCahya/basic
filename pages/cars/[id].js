6df2da0c0a0b0805643c7d34e075457eac3f099d
GustiCahya <gusticahyax@yahoo.com>
2022-07-06 08:30:34 +0700

    getStaticProps

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -1,9 +1,23 @@
 import { useRouter } from "next/router";
 
-export default function Car() {
+export default function Car({ car }) {
   const router = useRouter();
-  const { id } = router.query
-  return (
-    <h1>Mobil {id}</h1>
-  )
+  const { id } = router.query;
+  return <h1>Mobil {id}</h1>;
+}
+
+export async function getStaticProps() {
+  const car = {
+    id: "tesla"
+  };
+  return {
+    props: { car },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: {id: "Tesla"} }],
+    fallback: false,
+  };
 }
